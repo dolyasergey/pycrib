e29a823708ed2557a3f22db60c3cfd3df1da18de
dolyasergey <sergei.s.dolia@gmail.com>
2023-12-07 22:51:08 +0000

    Some other changes before starting

--- pycrib/utils.py
+++ pycrib/utils.py
@@ -1,19 +1,5 @@
 '''Helper functions'''
 
-def rate_to_odd(rate):
-	'''Converts probability from rate format into odd format
-	
-	Parameters
-	----------
-	rate : float
-		the probability of an event in rate format
-
-	Returns
-	-------
-	float
-		the probability of an event in odd format
-	'''
-	return (rate/(1 - rate))
 
 
 
